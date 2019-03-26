fcc9757d406ce651368eac03f8ac9a48d6ca4194
stcoyle <22sc01@cf.k12.mn.us>
2019-03-26 08:32:38 -0500

    Update FormulaStuff.java

--- Formula stuff/src/FormulaStuff.java
+++ Formula stuff/src/FormulaStuff.java
@@ -23,37 +23,45 @@ import org.apache.poi.xssf.usermodel.extensions.XSSFCellFill;
 
 public class FormulaStuff {
 	public static void main(String[] args) {
+		//creating some variables
 		String binaryString;
 		File f;
 		FileInputStream fis;
 		Workbook wb;
 		String b = "1";
 		try{
+			//Opening workbook
 			f = new File("C:\\Users\\samuel\\Documents\\Tupper's Formula\\Editable Formula2.xlsx");
 			fis = new FileInputStream(f);
 			wb = new XSSFWorkbook(fis);
 
+			//getting the binary value
 			XSSFSheet sheet1 = (XSSFSheet) wb.getSheetAt(1);
 			XSSFSheet sheet2 = (XSSFSheet) wb.getSheetAt(0);
 			b = sheet1.getRow(5).getCell(2).getStringCellValue();
 			
+			//Converting binary string to a very large num
 			binaryString = b.replace("	", "");
 			BigInteger bs = new BigInteger(binaryString,2);
 			bs = bs.multiply(new BigInteger("17"));
 			
+			//Format the cell to fit want format
 			XSSFCell cell = sheet2.createRow(25).createCell(2);
 			XSSFCellStyle cs = (XSSFCellStyle) wb.getCellStyleAt(1);
 			cs.setWrapText(true);
 			cs.setAlignment(HorizontalAlignment.CENTER);
 			cs.setVerticalAlignment(VerticalAlignment.CENTER);
-			
 			cell.setCellType(CellType.STRING);
 			cell.setCellStyle(cs);
+			
+			//Setting a cell to new k
 			cell.setCellValue(bs.toString());
 			
+			//Creating export location
 			String x = "fgh";
 			File f2 = new File("C:\\Users\\samuel\\Documents\\Tupper's Formula\\"+ x +".xlsx");
 			
+			//Putting changes on export location
 			FileOutputStream fileOut = new FileOutputStream(f2);
 			wb.write(fileOut);
 			fileOut.close();
@@ -61,7 +69,9 @@ public class FormulaStuff {
 			// Closing the workbook
 			wb.close();
 		}
+		
 		catch(IOException e) {
+			//Just in case
 			System.out.print(e);
 		}
 	}
